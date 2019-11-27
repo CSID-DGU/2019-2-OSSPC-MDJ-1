4155113d60bc35a104308e9bb700a63289c866de
Seokjin Choi <seokjinee@gmail.com>
2019-11-27 20:31:39 +0900

    git 코드 긁어와서 재시작!

--- src/pytris.py
+++ src/pytris.py
@@ -121,7 +121,7 @@ def draw_single_board(next, hold, score, level, goal):
             draw_block(dx, dy, ui_variables.t_color[matrix[x][y + 1]])
 
 # Draw multi board
-def draw_multi_board_1(next, hold, score, level, goal):
+def draw_multi_board(next, hold, score, level, goal):
     screen.fill(ui_variables.black)
     background_image_alpha()
 
@@ -806,7 +806,7 @@ while not done:
                 elif event.key == K_q:
                     done = True
                 # Hard drop
-                elif event.key == K_SPACE or event.key == K_LCTRL:
+                elif event.key == K_SPACE:
                     ui_variables.drop_sound.play()
                     while not is_bottom(dx, dy, mino, rotation):
                         dy += 1
@@ -816,7 +816,7 @@ while not done:
                     #draw_board(next_mino, hold_mino, score, level, goal)
                     draw_multi_board(next_mino, hold_mino, score, level, goal)
                 # Hold
-                elif event.key == K_LSHIFT or event.key == K_RSHIFT:
+                elif event.key == K_LSHIFT or event.key == K_c:
                     if hold == False:
                         ui_variables.move_sound.play()
                         if hold_mino == -1:
